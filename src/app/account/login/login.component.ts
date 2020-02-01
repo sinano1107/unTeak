62e51abda8e315c002930b72f087137ec63e92eb
百科おじさん <tftf161107@gmail.com>
2020-02-01 23:52:48 +0900

    ログインページ(仮)の追加

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Account } from '../../class/account';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  // アカウント型のaccountを作りinputを保存
+  public account = new Account();
 
   constructor() { }
 
