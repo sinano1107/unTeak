05cd8a83a3211a8d695631ee614cc9c2d5088de7
百科おじさん <tftf161107@gmail.com>
2020-02-02 16:16:54 +0900

    アクションLoadSessions、LoadSessionsSuccess、LoadSessionsFailの定義

--- src/app/core/store/actions/session.actions.ts
+++ src/app/core/store/actions/session.actions.ts
@@ -1,14 +1,33 @@
 import { Action } from '@ngrx/store';
+import { Session } from '../../../class/session';
 
 export enum SessionActionTypes {
   LoadSessions = '[Session] Load Sessions',
-  
-  
+  LoadSessionsSuccess = '[Session] Load Success',
+  LoadSessionsFail = '[Session] LoadFail',
 }
 
+// 読み込み時のアクション
 export class LoadSessions implements Action {
   readonly type = SessionActionTypes.LoadSessions;
 }
 
+// 読み込み成功のアクション
+export class LoadSessionsSuccess implements Action {
+  readonly type = SessionActionTypes.LoadSessionsSuccess;
 
-export type SessionActions = LoadSessions;
+  constructor(public payload: { session: Session }) {}
+}
+
+// 読み込み失敗時のアクション
+export class LoadSessionsFail implements Action {
+  readonly type = SessionActionTypes.LoadSessionsFail;
+
+  constructor(public payload?: { error: any }) {}
+}
+
+
+export type SessionActions =
+  | LoadSessions
+  | LoadSessionsSuccess
+  | LoadSessionsFail;
