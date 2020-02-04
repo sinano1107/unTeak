e8dc900d6176551a3460078c6d1d8ed17d9aba28
百科おじさん <tftf161107@gmail.com>
2020-02-04 16:57:11 +0900

    実行したサービスのログを追加

--- src/app/core/service/session.service.ts
+++ src/app/core/service/session.service.ts
@@ -14,6 +14,7 @@ export class SessionService {
 
   // ログイン状況確認
   checkLogin(): void {
+    console.debug('checkLogin実行--from(sessionService)');
     this.store.dispatch(new LoadSessions());
   }
 }
